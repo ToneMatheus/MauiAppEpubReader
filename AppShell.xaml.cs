be48b1963ac3b53f2d35cb058c6d47d54c04137d
aqw00 <matheus_tone@hotmail.com>
2024-11-26 10:36:19 +0100

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppEpubReader
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Models/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MauiAppEpubReader.Models
+{
+    public class MainViewModel
+    {
+        public ICommand CloseCommand { get; }
+
+        public MainViewModel()
+        {
+            CloseCommand = new Command(CloseApplication);
+        }
+
+        private void CloseApplication()
+        {
+            MauiProgram.CloseApplication();
+        }
+    }
+}
